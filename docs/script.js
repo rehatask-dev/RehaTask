eb35e349b72ce94217ea026ed240397a155f450f
rehatask-dev <yasuwind19850702@gmail.com>
2026-08-04 00:47:16 +0900

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,10 @@
+function addTask() {
+    const task = prompt("追加するタスクを入力してください");
 
+    if (task) {
+        const div = document.createElement("div");
+        div.innerHTML = `<label><input type="checkbox"> ${task}</label><br><br>`;
+
+        document.querySelector(".card").appendChild(div);
+    }
+}
